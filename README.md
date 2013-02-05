2b666245d5e2feb3a4377d28d2254e0123d428b1
Nick Peterson <tacticalsk8er@gmail.com>
2013-02-04 19:45:16 -0800

    Added Quit/Leave Message

--- README.md
+++ README.md
@@ -4,12 +4,12 @@ Join Message 2
 ###Features
 
 - Custom Join Message
-- *Custom Quit Message*
+- Custom Quit Message
 - Custom Group Join Message *
 - *Custom Group Quit Message* *
 - *Permission Prefix and Suffix Support* *
 - Formatting and Color Code Support
 
-**Requires Vault<br />
+*Requires Vault<br />
 *Not Implemented Yet*
  
--- src/me/tacticalsk8er/JoinMessage/Events.java
+++ src/me/tacticalsk8er/JoinMessage/Events.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class Events implements Listener{
 	
@@ -29,7 +30,21 @@ public class Events implements Listener{
 		//Formating
 		message = message.replaceAll("%player%", pName);
 		message = message.replaceAll("&((?i)[0-9a-fk-or])", "§$1");
-		
+		//Set Message
 		e.setJoinMessage(message);
+	}
+	
+	@EventHandler
+	public void onPlayerLeave(PlayerQuitEvent e){
+		//Initial Setup
+		Player p = e.getPlayer();
+		FileConfiguration config = plugin.getConfig();
+		String message = config.getString("QuitMessage");
+		String pName = p.getName();
+		//Formating
+		message = message.replaceAll("%player%", pName);
+		message = message.replaceAll("&((?i)[0-9a-fk-or])", "§$1");
+		//Set Message
+		e.setQuitMessage(message);
 	}
 }
