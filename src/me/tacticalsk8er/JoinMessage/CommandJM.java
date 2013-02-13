fce96c5a0a5a1d3a1a62dc3f36c9ba90e580fe55
Nick Peterson <tacticalsk8er@gmail.com>
2013-02-12 21:38:57 -0800

    Changes to command /jm

--- src/me/tacticalsk8er/JoinMessage/CommandJM.java
+++ src/me/tacticalsk8er/JoinMessage/CommandJM.java
@@ -23,22 +23,72 @@ public class CommandJM implements CommandExecutor{
 					if(sender.hasPermission("jm.edit.join")){
 						//Checks if a message is there
 						if(args.length > 2){
-							
+							StringBuilder sb = new StringBuilder();
+							for(int i = 2; i < args.length; i++){
+								sb.append(args[i]).append(' ');
+							}
+							plugin.getConfig().set("Message", sb.toString());
+							plugin.saveConfig();
+							return true;
 						} else {
-							sender.sendMessage(ChatColor.RED + "Not enought arguments!");
+							sender.sendMessage(ChatColor.RED + "Not enough arguments!");
 						}
 					}
 				} else if(args[1].equalsIgnoreCase("quit")){
 					//Checks if sender has permission
 					if(sender.hasPermission("jm.edit.quit")){
-						
+						//Checks if message is there
+						if(args.length > 2){
+							StringBuilder sb = new StringBuilder();
+							for(int i = 2; i < args.length; i++){
+								sb.append(args[i]).append(' ');
+							}
+							plugin.getConfig().set("QuitMessage", sb.toString());
+							plugin.saveConfig();
+							return true;
+						} else {
+							sender.sendMessage(ChatColor.RED + "Not enough arguments!");
+						}
 					}
 				} else if(args[1].equalsIgnoreCase("group")){
 					//Checks if sender has permission
 					if(sender.hasPermission("jm.edit.group")){
-						
+						if(args[2].equalsIgnoreCase("join")){
+							if(args.length > 4){
+								String group = args[3];
+								StringBuilder sb = new StringBuilder();
+								for(int i = 4; i < args.length; i++){
+									sb.append(args[i]).append(' ');
+								}
+								plugin.getConfig().set("Group." + group + ".Message", sb.toString());
+								plugin.saveConfig();
+								return true;
+							} else {
+								sender.sendMessage(ChatColor.RED + "Not enough arguments!");
+							}
+						} else if(args[2].equalsIgnoreCase("quit")){
+							if(args.length > 4){
+								String group = args[3];
+								StringBuilder sb = new StringBuilder();
+								for(int i = 4; i < args.length; i++){
+									sb.append(args[i]).append(' ');
+								}
+								plugin.getConfig().set("Group." + group + ".QuitMessage", sb.toString());
+								plugin.saveConfig();
+								return true;
+							} else {
+								sender.sendMessage(ChatColor.RED + "Not enough arguments!");
+							}
+						}
 					}
 				}
+			} else if(args[0].equalsIgnoreCase("reload")){
+				plugin.reloadConfig();
+				return true;
+			} else if(args[0].equalsIgnoreCase("help")){
+				//TODO Help pages
+			} else if(args.length == 0){
+				//TODO Copy help pages
 			}
 		}
 		return false;
